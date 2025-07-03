e8609929a976280dd30b7ad77f571c182e71c016
Pelda Kabak <peldakabak@hotmail.com>
2025-07-03 14:32:20 +0300

    Kategori controller düzenlendi , crud işlemleri tamamlandı

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar; // Assuming you have a Models folder with Siniflar namespace
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        Context c = new Context(); // Assuming you have a Context class for database operations
+        public ActionResult Index()
+        {
+            var urunler = c.Uruns.ToList(); // Fetching all products from the database
+            return View(urunler);
+        }
+    }
+}
